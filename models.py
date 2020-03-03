ee8f1b746989cf525e91b0d54597243cfcd0c360
Khoong Wei Hao <khoongweihao@hotmail.com>
2020-03-03 23:25:21 +0800

    added models script containing BUSU-Net and LightBUSU-Net

---
+++ models.py
@@ -0,0 +1,240 @@
+from __future__ import division
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, concatenate, Conv2D, MaxPooling2D, UpSampling2D, Reshape, Lambda, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from tensorflow.keras import backend as K
+from tensorflow.keras.utils import plot_model as plot
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.optimizers import *
+from tensorflow.keras.layers import *        
+import numpy as np
+
+# BUSU-Net with 1 big-U 1 small-U, light version (less deep U)
+def LightBUSU_Net(input_size = (256,256,1)):
+    N = input_size[0]
+    inputs = Input(input_size) 
+    
+    #############################
+    ### First U-net with LSTM ###
+    #############################
+
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+  
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    # D1
+    conv4 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)     
+    conv4_1 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+    drop4_1 = Dropout(0.5)(conv4_1)   
+    
+    up6 = Conv2DTranspose(128, kernel_size=2, strides=2, padding='same',kernel_initializer = 'he_normal')(drop4_1)
+    up6 = BatchNormalization(axis=3)(up6)
+    up6 = Activation('relu')(up6)
+
+    x1 = Reshape(target_shape=(1, np.int32(N/2), np.int32(N/2), 128))(conv2)
+    x2 = Reshape(target_shape=(1, np.int32(N/2), np.int32(N/2), 128))(up6)
+    merge6  = concatenate([x1,x2], axis = 1) 
+    merge6 = ConvLSTM2D(filters = 64, kernel_size=(3, 3), padding='same', return_sequences = False, go_backwards = True,kernel_initializer = 'he_normal' )(merge6)
+            
+    conv6 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+    conv6 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+
+    up7 = Conv2DTranspose(64, kernel_size=2, strides=2, padding='same',kernel_initializer = 'he_normal')(conv6)
+    up7 = BatchNormalization(axis=3)(up7)
+    up7 = Activation('relu')(up7)
+
+    x1 = Reshape(target_shape=(1, N, N, 64))(conv1)
+    x2 = Reshape(target_shape=(1, N, N, 64))(up7)
+    merge8  = concatenate([x1,x2], axis = 1) 
+    merge8 = ConvLSTM2D(filters = 32, kernel_size=(3, 3), padding='same', return_sequences = False, go_backwards = True,kernel_initializer = 'he_normal' )(merge8)    
+    
+    conv8 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+    
+    ##############################
+    ### Second U-net with LSTM ###
+    ##############################
+
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    # D1
+    conv4 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)     
+    conv4_1 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+    drop4_1 = Dropout(0.5)(conv4_1)   
+    
+    up6 = Conv2DTranspose(64, kernel_size=2, strides=2, padding='same',kernel_initializer = 'he_normal')(drop4_1)
+    up6 = BatchNormalization(axis=3)(up6)
+    up6 = Activation('relu')(up6)
+
+    x1 = Reshape(target_shape=(1, N, N, 64))(conv1)
+    x2 = Reshape(target_shape=(1, N, N, 64))(up6)
+    merge6  = concatenate([x1,x2], axis = 1) 
+    merge6 = ConvLSTM2D(filters = 32, kernel_size=(3, 3), padding='same', return_sequences = False, go_backwards = True,kernel_initializer = 'he_normal' )(merge6)
+            
+    conv8 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+    conv8 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+
+    conv8 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+    conv9 = Conv2D(1, 1, activation = 'sigmoid')(conv8)
+
+    model = Model(inputs, conv9)
+    model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return model
+
+# Big-U Small-U net, BDCLU-net with 'deeper' U architecture
+def BUSU_Net(input_size = (256,256,1)):
+    N = input_size[0]
+    inputs = Input(input_size) 
+    
+    #############################
+    ### First U-net with LSTM ###
+    #############################
+
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+  
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+    drop3 = Dropout(0.5)(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    # D1
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)     
+    conv4_1 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+    drop4_1 = Dropout(0.5)(conv4_1)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4_1)
+    # D2
+    conv4_2 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)     
+    conv4_2 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4_2)
+    conv4_2 = Dropout(0.5)(conv4_2)
+    # D2'
+    conv4_22 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4_2)     
+    conv4_22 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4_22)
+    conv4_22 = Dropout(0.5)(conv4_22)
+    # D3
+    merge_dense = concatenate([conv4_22,conv4_2], axis = 3)
+    conv4_3 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge_dense)     
+    conv4_3 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4_3)
+    drop4_3 = Dropout(0.5)(conv4_3)
+    up5 = Conv2DTranspose(512, kernel_size=3, strides=2, padding='same',kernel_initializer = 'he_normal')(drop4_3)
+    up5 = BatchNormalization(axis=3)(up5)
+    up5 = Activation('relu')(up5)
+
+    x1 = Reshape(target_shape=(1, np.int32(N/8), np.int32(N/8), 512))(drop4_1)
+    x2 = Reshape(target_shape=(1, np.int32(N/8), np.int32(N/8), 512))(up5)
+    merge5  = concatenate([x1,x2], axis = 1) 
+    merge5 = ConvLSTM2D(filters = 256, kernel_size=(3, 3), padding='same', return_sequences = False, go_backwards = True,kernel_initializer = 'he_normal' )(merge5)
+    
+    conv5 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge5)     
+    conv5 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+
+    up6 = Conv2DTranspose(256, kernel_size=3, strides=2, padding='same',kernel_initializer = 'he_normal')(conv5)
+    up6 = BatchNormalization(axis=3)(up6)
+    up6 = Activation('relu')(up6)
+
+    x1 = Reshape(target_shape=(1, np.int32(N/4), np.int32(N/4), 256))(drop3)
+    x2 = Reshape(target_shape=(1, np.int32(N/4), np.int32(N/4), 256))(up6)
+    merge6  = concatenate([x1,x2], axis = 1) 
+    merge6 = ConvLSTM2D(filters = 128, kernel_size=(3, 3), padding='same', return_sequences = False, go_backwards = True,kernel_initializer = 'he_normal' )(merge6)
+            
+    conv6 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+    conv6 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+
+    up7 = Conv2DTranspose(128, kernel_size=3, strides=2, padding='same',kernel_initializer = 'he_normal')(conv6)
+    up7 = BatchNormalization(axis=3)(up7)
+    up7 = Activation('relu')(up7)
+
+    x1 = Reshape(target_shape=(1, np.int32(N/2), np.int32(N/2), 128))(conv2)
+    x2 = Reshape(target_shape=(1, np.int32(N/2), np.int32(N/2), 128))(up7)
+    merge7  = concatenate([x1,x2], axis = 1) 
+    merge7 = ConvLSTM2D(filters = 64, kernel_size=(3, 3), padding='same', return_sequences = False, go_backwards = True,kernel_initializer = 'he_normal' )(merge7)
+        
+    conv7 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+    conv7 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+
+    up8 = Conv2DTranspose(64, kernel_size=3, strides=2, padding='same',kernel_initializer = 'he_normal')(conv7)
+    up8 = BatchNormalization(axis=3)(up8)
+    up8 = Activation('relu')(up8)    
+
+    x1 = Reshape(target_shape=(1, N, N, 64))(conv1)
+    x2 = Reshape(target_shape=(1, N, N, 64))(up8)
+    merge8  = concatenate([x1,x2], axis = 1) 
+    merge8 = ConvLSTM2D(filters = 32, kernel_size=(3, 3), padding='same', return_sequences = False, go_backwards = True,kernel_initializer = 'he_normal' )(merge8)    
+    
+    conv8 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+    
+    ##############################
+    ### Second U-net with LSTM ###
+    ##############################
+
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+  
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+    drop3 = Dropout(0.5)(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    # D1
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)     
+    conv4_1 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+    drop4_1 = Dropout(0.5)(conv4_1)
+    # D2
+    conv4_2 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(drop4_1)     
+    conv4_2 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4_2)
+    conv4_2 = Dropout(0.5)(conv4_2)
+    # D3
+    merge_dense = concatenate([conv4_2,drop4_1], axis = 3)
+    conv4_3 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge_dense)     
+    conv4_3 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4_3)
+    drop4_3 = Dropout(0.5)(conv4_3)
+    up6 = Conv2DTranspose(256, kernel_size=3, strides=2, padding='same',kernel_initializer = 'he_normal')(drop4_3)
+    up6 = BatchNormalization(axis=3)(up6)
+    up6 = Activation('relu')(up6)
+
+    x1 = Reshape(target_shape=(1, np.int32(N/4), np.int32(N/4), 256))(drop3)
+    x2 = Reshape(target_shape=(1, np.int32(N/4), np.int32(N/4), 256))(up6)
+    merge6  = concatenate([x1,x2], axis = 1) 
+    merge6 = ConvLSTM2D(filters = 128, kernel_size=(3, 3), padding='same', return_sequences = False, go_backwards = True,kernel_initializer = 'he_normal' )(merge6)
+            
+    conv6 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+    conv6 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+
+    up7 = Conv2DTranspose(128, kernel_size=3, strides=2, padding='same',kernel_initializer = 'he_normal')(conv6)
+    up7 = BatchNormalization(axis=3)(up7)
+    up7 = Activation('relu')(up7)
+
+    x1 = Reshape(target_shape=(1, np.int32(N/2), np.int32(N/2), 128))(conv2)
+    x2 = Reshape(target_shape=(1, np.int32(N/2), np.int32(N/2), 128))(up7)
+    merge7  = concatenate([x1,x2], axis = 1) 
+    merge7 = ConvLSTM2D(filters = 64, kernel_size=(3, 3), padding='same', return_sequences = False, go_backwards = True,kernel_initializer = 'he_normal' )(merge7)
+        
+    conv7 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+    conv7 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+
+    up8 = Conv2DTranspose(64, kernel_size=3, strides=2, padding='same',kernel_initializer = 'he_normal')(conv7)
+    up8 = BatchNormalization(axis=3)(up8)
+    up8 = Activation('relu')(up8)    
+
+    x1 = Reshape(target_shape=(1, N, N, 64))(conv1)
+    x2 = Reshape(target_shape=(1, N, N, 64))(up8)
+    merge8  = concatenate([x1,x2], axis = 1) 
+    merge8 = ConvLSTM2D(filters = 32, kernel_size=(3, 3), padding='same', return_sequences = False, go_backwards = True,kernel_initializer = 'he_normal' )(merge8)    
+    
+    conv8 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+    conv8 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+
+    conv8 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+    conv9 = Conv2D(1, 1, activation = 'sigmoid')(conv8)
+
+    model = Model(inputs, conv9)
+    model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return model
